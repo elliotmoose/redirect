24d904a705ba285a346b3a38b669b97792d2f942
Elliot Koh <appdev.moose@gmail.com>
2020-10-02 19:32:26 +0800

    Final concert link updated

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const redirectComp = () => {
   React.useEffect(()=>{
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    window.location.href = "https://www.youtube.com/watch?v=jOiwqAOgh7Q"
   })
   return <div/>
 }
